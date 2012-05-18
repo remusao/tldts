b3b9d385492b807eab753c490b0d81d8a54c5799
oncletom <thomas@oncle-tom.net>
2012-05-18 19:27:31 +0200

    Rule parser

---
+++ test/rule.js
@@ -0,0 +1,16 @@
+var rule = require('../lib/rule.js');
+var expect = require('expect.js');
+
+suite('Public Suffix Rule', function(){
+  suite('#parse()', function(){
+
+  });
+
+  suite('#domainBuilder()', function(){
+
+  });
+
+  suite('#filterRow()', function(){
+
+  });
+});
