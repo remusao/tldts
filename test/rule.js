aba569a5ce0e013124ec15edd23bc858c68cbb03
oncletom <thomas@oncle-tom.net>
2013-01-01 16:17:57 +0100

    Constructor testing

--- test/rule.js
+++ test/rule.js
@@ -3,35 +3,59 @@
 /*jshint node:true strict: true */
 /*global suite:true test:true suiteSetup:true */
 
-var rule = require('../lib/rule.js');
+var Rule = require(__dirname + '/../lib/rule.js');
 var expect = require('expect.js');
 
 suite('Public Suffix Rule', function(){
+  var rules;
+
   suiteSetup(function(){
+    rules = {
+      tld: new Rule({firstLevel: 'com'}),
+      sld: new Rule({firstLevel: 'com', 'secondLevel': 'uk'}),
+      tldWildcard: new Rule({firstLevel: 'om', wildcard: true}),
+      sldException: new Rule({firstLevel: 'om', secondLevel: 'songfest', exception: true}),
+      sldWildcard: new Rule({firstLevel: 'om', secondLevel: 'fake', wildcard: true})
+    };
+  });
+
+  test('#constructor', function(){
+    expect(rules.tld.secondLevel).to.be(null);
+    expect(rules.tld.wildcard).to.be(false);
+    expect(rules.tld.exception).to.be(false);
+
+    expect(rules.sld.secondLevel).to.be('uk');
+
+    expect(rules.tldWildcard.secondLevel).to.be(null);
+    expect(rules.tldWildcard.wildcard).to.be(true);
+    expect(rules.tldWildcard.exception).to.be(false);
+
+    expect(rules.sldException.secondLevel).to.be('songfest');
+    expect(rules.sldException.wildcard).to.be(false);
+    expect(rules.sldException.exception).to.be(true);
+
+    expect(rules.sldWildcard.secondLevel).to.be('fake');
+    expect(rules.sldWildcard.wildcard).to.be(true);
+    expect(rules.sldWildcard.exception).to.be(false);
+  });
+
+  test('#getNormalXld()', function(){
 
   });
 
-  suite('#constructor', function(){
+  test('#getNormalPattern()', function(){
 
   });
 
-  suite('#getNormalXld()', function(){
+  test('#getWildcardPattern()', function(){
 
   });
 
-  suite('#getNormalPattern()', function(){
+  test('#getExceptionPattern()', function(){
 
   });
 
-  suite('#getWildcardPattern()', function(){
-
-  });
-
-  suite('#getExceptionPattern()', function(){
-
-  });
-
-  suite('#getPattern()', function(){
+  test('#getPattern()', function(){
 
   });
 });
