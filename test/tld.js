7bc1a3d33534e7f5bc317190c23fb2cc3ce69a5e
oncletom <thomas@oncle-tom.net>
2013-01-07 16:58:32 +0100

    Adjusted tests to provide more sense to the method

--- test/tld.js
+++ test/tld.js
@@ -59,19 +59,25 @@ suite('tld.js', function () {
     });
   });
 
-  suite('#domainExists', function () {
-    test('existing domain', function () {
-      expect(tld.domainExists('com')).to.be(true);
-      expect(tld.domainExists('uk.com')).to.be(true);
-      expect(tld.domainExists('co.uk')).to.be(true);
-      expect(tld.domainExists('台灣')).to.be(true);
+  suite('#tldExists', function () {
+    test('existing TLD', function () {
+      expect(tld.tldExists('com')).to.be(true);
+      expect(tld.tldExists('example.com')).to.be(true);
+      expect(tld.tldExists('co.uk')).to.be(true);
+      expect(tld.tldExists('amazon.co.uk')).to.be(true);
+      expect(tld.tldExists('台灣')).to.be(true);
+      expect(tld.tldExists('台灣.台灣')).to.be(true);
     });
 
-    test('inexisting domain', function () {
-      expect(tld.domainExists('con')).to.be(false);
-      expect(tld.domainExists('google.com')).to.be(false);
-      expect(tld.domainExists('go')).to.be(false);
-      expect(tld.domainExists('チーズ')).to.be(false);
+    test('unexisting TLD', function () {
+      expect(tld.tldExists('con')).to.be(false);
+      expect(tld.tldExists('example.con')).to.be(false);
+      expect(tld.tldExists('go')).to.be(false);
+      expect(tld.tldExists('チーズ')).to.be(false);
+    });
+
+    test('they cannot be verified', function(){
+      expect(tld.tldExists('uk.com')).to.be(true);
     });
   });
 });
