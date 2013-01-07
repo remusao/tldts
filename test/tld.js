33192a7fe59f9142b384ae73c8c7ac165f3e5515
oncletom <thomas@oncle-tom.net>
2013-01-07 15:28:16 +0100

    Reformatting

--- test/tld.js
+++ test/tld.js
@@ -6,52 +6,53 @@
 var tld = require('../index.js');
 var expect = require('expect.js');
 
-suite('tld.js', function(){
-  suite('Basics', function(){
-    test('Rules are already loaded', function(){
+suite('tld.js', function () {
+  suite('Basics', function () {
+    test('Rules are already loaded', function () {
       expect(tld.rules).to.be.an(Object);
       expect(Object.keys(tld.rules).length).to.be.above(0);
     });
   });
 
-  suite('#isValid()', function(){
-    test('Good ones', function(){
+  suite('#isValid()', function () {
+    test('Good ones', function () {
       expect(tld.isValid('')).to.be(false);
       expect(tld.isValid('google.com')).to.be(true);
       expect(tld.isValid('miam.google.com')).to.be(true);
       expect(tld.isValid('miam.miam.google.com')).to.be(true);
     });
-    test('Invalid types', function(){
+    test('Invalid types', function () {
       expect(tld.isValid(null)).to.be(false);
       expect(tld.isValid(undefined)).to.be(false);
       expect(tld.isValid(0)).to.be(false);
       expect(tld.isValid([])).to.be(false);
       expect(tld.isValid({})).to.be(false);
-      expect(tld.isValid(function(){})).to.be(false);
+      expect(tld.isValid(function () {
+      })).to.be(false);
     });
 
-    test('Invalid notation', function(){
+    test('Invalid notation', function () {
       expect(tld.isValid('.google.com')).to.be(false);
       expect(tld.isValid('.com')).to.be(false);
     });
 
-    test('Dot-less hostname', function(){
+    test('Dot-less hostname', function () {
       expect(tld.isValid('localhost')).to.be(false);
       expect(tld.isValid('google')).to.be(false);
     });
   });
 
-  suite('#getDomain()', function(){
-    test('basic domains', function(){
+  suite('#getDomain()', function () {
+    test('basic domains', function () {
       expect(tld.getDomain('google.com')).to.be('google.com');
       expect(tld.getDomain('t.co')).to.be('t.co');
     });
 
-    test('composed ', function() {
+    test('composed ', function () {
       expect(tld.getDomain('google.co.uk')).to.be('google.co.uk');
     });
 
-    test('subdomains', function() {
+    test('subdomains', function () {
       expect(tld.getDomain('fr.google.com')).to.be('google.com');
       expect(tld.getDomain('foo.google.co.uk')).to.be('google.co.uk');
       expect(tld.getDomain('fr.t.co')).to.be('t.co');
