127ed124368029996de25b69dce4fb0df70bcd35
Thomas Parisot <thomas.parisot@bbc.co.uk>
2013-10-09 15:30:41 +0100

    Removing useless grunt tasks
    - calling directly jshint
    - removing default es5
    - running again jshint

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,23 +2,9 @@
 
 module.exports = function (grunt) {
   // Project configuration.
-  grunt.initConfig({
-    jshint: {
-      all: ['grunt.js', 'lib/**/*.js', 'test/**/*.js'],
-      options: {
-        jshintrc: '.jshintrc'
-      }
-    },
-    watch: {
-      files: '<config:lint.files>',
-      tasks: 'lint'
-    }
-  });
+  grunt.initConfig({});
 
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-
-  grunt.registerTask('default', 'jshint');
+  grunt.registerTask('default', []);
 
   // Custom Task to build files
   grunt.loadTasks('lib/grunt');
