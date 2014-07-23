a7cadff5745c340702c8f7fbc0c012ccf09298a2
Thomas Parisot <thomas.parisot@bbc.co.uk>
2014-07-23 12:30:06 +0100

    Proper use of `path.join` in the updater.

--- bin/update.js
+++ bin/update.js
@@ -4,7 +4,7 @@
 
 var pathJoin = require('path').join;
 
-var defaultConfig = require(pathJoin(__dirname, '/defaults.json'));
-var updater = require(pathJoin(__dirname, '/../lib/updater/rules.js'))(defaultConfig);
+var defaultConfig = require(pathJoin(__dirname, 'defaults.json'));
+var updater = require(pathJoin(__dirname, '..', 'lib', 'updater', 'rules.js'))(defaultConfig);
 
 updater.run();
