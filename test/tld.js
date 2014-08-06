olivoil/tld.js

Added some tests for the new method `getPublicSuffix`

test/tld.js
@@ -59,6 +59,13 @@ suite('tld.js', function () {
       expect(tld.getDomain('foo.google.co.uk')).to.be('google.co.uk');
       expect(tld.getDomain('fr.t.co')).to.be('t.co');
     });
+
+    //@see https://github.com/oncletom/tld.js/issues/25
+    //@see https://github.com/oncletom/tld.js/issues/30
+    test('existing rule constraint', function () {
+      expect(tld.getDomain('s3.amazonaws.com')).to.be(null);
+      expect(tld.getDomain('blogspot.co.uk')).to.be(null);
+    });
   });
 
   suite('#tldExists', function () {
@@ -80,6 +87,14 @@ suite('tld.js', function () {
 
     test('they cannot be verified', function(){
       expect(tld.tldExists('uk.com')).to.be(true);
+    });
+  });
+
+  suite('#getPublicSuffix', function () {
+    //@see https://github.com/oncletom/tld.js/issues/30
+    test('existing rule constraint', function () {
+      expect(tld.getPublicSuffix('s3.amazonaws.com')).to.be('s3.amazonaws.com');
+      expect(tld.getPublicSuffix('blogspot.co.uk')).to.be('blogspot.co.uk');
     });
   });
 

remusao/tldts

Conflicts:
	test/tld.js

test/tld.js
@@ -70,6 +70,13 @@ describe('tld.js', function () {
       expect(tld.getDomain('www.bl.uk')).to.equal('bl.uk');
       expect(tld.getDomain('www.majestic12.co.uk')).to.equal('majestic12.co.uk');
     });
+
+    //@see https://github.com/oncletom/tld.js/issues/25
+    //@see https://github.com/oncletom/tld.js/issues/30
+    it('existing rule constraint', function () {
+      expect(tld.getDomain('s3.amazonaws.com')).to.be(null);
+      expect(tld.getDomain('blogspot.co.uk')).to.be(null);
+    });
   });
 
   describe('tldExists method', function () {
@@ -91,6 +98,17 @@ describe('tld.js', function () {
 
     it('should be truthy on complex TLD which cannot be verified as long as the gTLD exists', function(){
       expect(tld.tldExists('uk.com')).to.be(true);
+    });
+  });
+
+  describe('#getPublicSuffix', function () {
+    //@see https://github.com/oncletom/tld.js/issues/30
+    it('existing rule constraint', function () {
+      expect(tld.getPublicSuffix('s3.amazonaws.com')).to.be('s3.amazonaws.com');
+    });
+
+    it('existing rule constraint', function () {
+      expect(tld.getPublicSuffix('www.google.co.uk')).to.be('google.co.uk');
     });
   });
 