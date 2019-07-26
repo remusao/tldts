c6e7f3bc9b3db9820960b2a36a0e3d7891ebf715
Rémi Berson <remi@cliqz.com>
2019-07-26 13:43:13 +0200

    update project structure
    * speed-up build using composite mode for TypeScript
    * fix source maps generation
    * make sure tslib does not appear in build artifacts

--- packages/tldts-experimental/rollup.config.ts
+++ packages/tldts-experimental/rollup.config.ts
@@ -1,34 +1,34 @@
 import compiler from '@ampproject/rollup-plugin-closure-compiler';
 import resolve from 'rollup-plugin-node-resolve';
+import sourcemaps from 'rollup-plugin-sourcemaps';
 
 export default [
-  // ES6 + UMD + CommonJS minified
+  // CommonJS
   {
-    input: './build/es6/index.js',
-    output: [
-      {
-        file: './dist/index.esm.min.js',
-        format: 'es',
-        sourcemap: true,
-      },
-      {
-        file: './dist/index.cjs.min.js',
-        format: 'cjs',
-        sourcemap: true,
-      },
-      {
-        file: './dist/index.umd.min.js',
-        format: 'umd',
-        name: 'tldts',
-        sourcemap: true,
-      },
-    ],
+    input: './dist/es6/index.js',
+    output: {
+      file: './dist/cjs/index.js',
+      format: 'cjs',
+      sourcemap: true,
+    },
     plugins: [
       resolve(),
-      compiler({
-        language_out: 'NO_TRANSPILE',
-        warning_level: 'DEFAULT',
-      }),
+      sourcemaps(),
+    ],
+  },
+  // UMD minified
+  {
+    input: './dist/es6/index.js',
+    output: {
+      file: './dist/index.umd.min.js',
+      format: 'umd',
+      name: 'tldts',
+      sourcemap: true,
+    },
+    plugins: [
+      resolve(),
+      compiler(),
+      sourcemaps(),
     ],
   },
 ];
