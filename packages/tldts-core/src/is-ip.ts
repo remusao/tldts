1da8e5a4049c47e25acf4f4a81a301c096b39f51
Rémi <remusao@users.noreply.github.com>
2023-03-19 09:32:43 +0100

    Update packages/tldts-core/src/is-ip.ts

--- packages/tldts-core/src/is-ip.ts
+++ packages/tldts-core/src/is-ip.ts
@@ -65,7 +65,7 @@ function isProbablyIpv6(hostname: string): boolean {
       !(
         (code >= 48 && code <= 57) || // 0-9
         (code >= 97 && code <= 102) || // a-f
-        (code >= 65 && code <= 90)
+        (code >= 65 && code <= 90) // A-F
       )
     ) {
       return false;
